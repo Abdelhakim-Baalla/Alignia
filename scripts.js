ae0dbe96acda1713f67ad8d5bd90104d881903dc
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-09-16 14:28:05 +0100

    Refactor la fonction createGrid pour accepter un paramètre de taille et améliore la structure CSS pour la grille

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
-// Définir la variable n pour l'initialisation de la longueur et la largeur des grilles
-let n = 3;
-
 // Fonction pour générer les grilles de jeu
-function createGrid(){
-    const grid = [];
-    let grille = document.getElementById("grille");
-    for(let i = 0; i < n; i++){
-        grid[i] = [];
+function createGrid(n) {
+  const grid = [];
+  let grille = document.getElementById("grille");
+  grille.innerHTML = "";
+  for (let i = 0; i < n; i++) {
+    grid[i] = [];
 
-        for(let j = 0; j < n; j++){
-            grid[i][j] = '<div style="border: 1px solid black; width: 50px; height: 50px; display: inline-block; text-align: center;">0</div>';
-           
-            grille.innerHTML += grid[i][j];
-        }
-        grille.style.gridTemplateColumns = `repeat(${n}, 50px)`;
+    for (let j = 0; j < n; j++) {
+      grid[i][j] =
+        '<div style="border: 1px solid black; width: 50px; height: 50px; display: inline-block; text-align: center;">0</div>';
+
+      grille.innerHTML += grid[i][j];
     }
-    // return grid;
+    grille.style.gridTemplateColumns = `repeat(${n}, 50px)`;
+  }
+  // return grid;
 }
 
-createGrid();
+// createGrid();
 
-function changerTaille(){
-    let NewTaille = document.getElementById("taille").value;
-    n = NewTaille;
-    createGrid();
-    // console.log(NewTaille);
+function changerTaille() {
+  let NewTaille = document.getElementById("taille").value;
+  n = parseInt(NewTaille);
+  createGrid(n);
+  // console.log(NewTaille);
 }
