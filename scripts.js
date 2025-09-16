d773c41bd6a9236428639c6cde97875f9d41db5d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-09-16 12:21:53 +0100

    Modification de la variable n pour ajuster la taille de la grille et amélioration de la structure HTML pour l'affichage des cellules.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,20 @@
 // Définir la variable n pour l'initialisation de la longueur et la largeur des grilles
-let n = 5;
+let n = 3;
 
 // Fonction pour générer les grilles de jeu
 function createGrid(){
     const grid = [];
+    let grille = document.getElementById("grille");
     for(let i = 0; i < n; i++){
         grid[i] = [];
 
         for(let j = 0; j < n; j++){
-            grid[i][j] = '<span>0</span>';
+            grid[i][j] = '<div style="border: 1px solid black; width: 50px; height: 50px; display: inline-block; text-align: center;">0</div>';
             if(j == n - 1) grid[i][j] += '<br>';
+            grille.innerHTML += grid[i][j];
         }
     }
-    return grid;
+    // return grid;
 }
 
-document.write(createGrid());
+createGrid();
