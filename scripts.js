acc5a8548dc73fd7cda997d724c2a8edd3f832d0
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-09-16 12:14:02 +0100

    Correction de la variable n pour l'initialisation de la longueur et la largeur des grilles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-// Difinir le variable n pour l'nitialisation de la longeur et la largeur des grilles
-let n = 3;
+// Définir la variable n pour l'initialisation de la longueur et la largeur des grilles
+let n = 5;
 
+// Fonction pour générer les grilles de jeu
+function createGrid(){
+    const grid = [];
+    for(let i = 0; i < n; i++){
+        grid[i] = [];
+
+        for(let j = 0; j < n; j++){
+            grid[i][j] = '<span>0</span>';
+            if(j == n - 1) grid[i][j] += '<br>';
+        }
+    }
+    return grid;
+}
+
+document.write(createGrid());
