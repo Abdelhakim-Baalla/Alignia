6cf453c3136dd895b33cb2acbef609eb5a8c080d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-09-16 14:56:57 +0100

    Ajout des attributs 'for' aux labels et mise à jour des valeurs minimales des champs de saisie. Ajout de la classe 'box-player' aux divs de la grille pour une meilleure gestion des événements.

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ function createGrid(n) {
 
     for (let j = 0; j < n; j++) {
       grid[i][j] =
-        '<div style="border: 1px solid black; width: 50px; height: 50px; display: inline-block; text-align: center;"></div>';
+        '<div style="border: 1px solid black; width: 50px; height: 50px; display: inline-block; text-align: center;" class="box-player"></div>';
 
       grille.innerHTML += grid[i][j];
     }
@@ -21,10 +21,18 @@ function createGrid(n) {
 
 function changerTaille() {
   let NewTaille = document.getElementById("taille").value;
+  let k = document.getElementById("nbr-lignes-a-gagner").value;
   n = parseInt(NewTaille);
+
   createGrid(n);
   // console.log(NewTaille);
 }
 
-let appliqueBtn = document.getElementById('appliquer');
-appliqueBtn.addEventListener('click', changerTaille);
+let appliqueBtn = document.getElementById("appliquer");
+appliqueBtn.addEventListener("click", changerTaille);
+
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("box-player")) {
+      console.log(event.target.classList);
+  }
+});
